b67df92cacbe0dd578aeb559f30cc146c96c00a3
MohammedBayatena <mohammedbayatena@outlook.com>
2025-08-29 00:46:56 +0300

    update docs deployment

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -14,7 +14,7 @@ export default {
   assets: {
     'index.csr.html': {size: 721, hash: '5c35fbb8d349c1f5fd8fa68e6a9ac9213cacd80dff726a0c2f858fb4f73718dc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 1123, hash: 'c4e571cec5a9e7b0ab54a571051c0413ae45b646a2f5ce199a41a360591def6d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 23777, hash: '83eaaee401a4b4d2ecbebd9cca1c7cf121c893d92d5506d337f01bbd3c4f8d00', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.html': {size: 23777, hash: 'e27b02f089787e34462e201918c89a6e64bc1d8698c461ccf9aad7477d64d12f', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-ZZ3MOTG6.css': {size: 1002, hash: 'Q5uzkgaRXXo', text: () => import('./assets-chunks/styles-ZZ3MOTG6_css.mjs').then(m => m.default)}
   },
 };
