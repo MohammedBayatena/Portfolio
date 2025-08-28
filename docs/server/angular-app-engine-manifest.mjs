ab62f0dc11a1cc274e09a9221b1ddbd46a65ebf8
MohammedBayatena <mohammedbayatena@outlook.com>
2025-08-29 00:35:26 +0300

    Try Production Deployment Configuration

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: 'Portfolio',
+  basePath: 'https://mohammedbayatena.github.io/Portfolio',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,7 +2,7 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: 'Portfolio',
+  baseHref: 'https://mohammedbayatena.github.io/Portfolio/',
   locale: undefined,
   routes: [
   {
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 685, hash: '0c1819e4bf9351b12904540a7e4a35ec1a9dd4bcc28c041c88b3d3feb7c2e674', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1087, hash: 'd4808642d7953bd4ee25d304e40dc27f319d73781a71c5afe3b8c261a6921047', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 24450, hash: '3c8ef8ebea11e6a4f026fb67c09265c7f90a1276400386cb4a25a5d1cfced77c', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 721, hash: '5c35fbb8d349c1f5fd8fa68e6a9ac9213cacd80dff726a0c2f858fb4f73718dc', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1123, hash: 'c4e571cec5a9e7b0ab54a571051c0413ae45b646a2f5ce199a41a360591def6d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 23777, hash: '83eaaee401a4b4d2ecbebd9cca1c7cf121c893d92d5506d337f01bbd3c4f8d00', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-ZZ3MOTG6.css': {size: 1002, hash: 'Q5uzkgaRXXo', text: () => import('./assets-chunks/styles-ZZ3MOTG6_css.mjs').then(m => m.default)}
   },
 };
